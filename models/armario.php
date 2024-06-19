70fd9e102c1537f9e134a44d2aaabd6e891e93e8
unknown <lraulino26@gmail.com>
2024-06-19 18:22:40 -0300

    testando armarios

--- models/armario.php
+++ models/armario.php
@@ -46,7 +46,23 @@ class Armario {
 
     public function SelecionarTodosArmarios() {
 
-        include("bancoDeDados/conectarNoBancoClasse.php");
+         // PHP Data Objects(PDO) Sample Code:
+         try {
+            $conn = new PDO("sqlsrv:server = tcp:testes-aplicacoes.database.windows.net,1433; Database = testes", "teste", "qwe456123!");
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            print ("Error connecting to SQL Server.");
+            die(print_r($e));
+        }
+
+        // SQL Server Extension Sample Code:
+        $connectionInfo = array("UID" => "teste", "pwd" => "qwe456123!", "Database" => "testes", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+        $serverName = "tcp:testes-aplicacoes.database.windows.net,1433";
+        $conn = sqlsrv_connect($serverName, $connectionInfo);
+        if ($conn === false) {
+            die(print_r(sqlsrv_errors(), true));
+        }
+        
         $armarioSelecionado = sqlsrv_query($conn, "SELECT * FROM armario");
 
         return $armarioSelecionado;
