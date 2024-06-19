640bdc5f5e9362df33d30d65bea6dee09c9d04c6
unknown <lraulino26@gmail.com>
2024-06-19 18:27:01 -0300

    testando armarios

--- views/armariosView.php
+++ views/armariosView.php
@@ -1,82 +1,102 @@
-<?php include_once('public/cabecalho.php'); ?>
-<?php require_once('controllers/armarioController.php'); ?>
+<?php include_once ('public/cabecalho.php'); ?>
+<?php require_once ('controllers/armarioController.php'); ?>
 
 <title>Armários | Lockertec</title>
 
 </head>
+
 <body>
 
-    <?php include_once('public/navbar.php')?>
-    <?php include_once('bancoDeDados/conectarNoBanco.php')?>
+    <?php include_once ('public/navbar.php') ?>
+    <?php include_once ('bancoDeDados/conectarNoBanco.php') ?>
 
     <div class="container col-11 Armarios">
         <h2>Armários</h2>
         <form action="index.php" method="GET">
-        <input type="hidden" name="view" value="armarios">    
-        <div class="container row mt-5" id="filtros">
-            <div class="col-5">
-                <div class="form-floating">
-                    <select class="form-select" id="Andar" name="andar">
-                        <option value="Primeiro" selected>Primeiro</option>
-                        <option value="Segundo">Segundo</option>
-                    </select>
-                    <label for="Andar" class="form-label">Andar</label>
+            <input type="hidden" name="view" value="armarios">
+            <div class="container row mt-5" id="filtros">
+                <div class="col-5">
+                    <div class="form-floating">
+                        <select class="form-select" id="Andar" name="andar">
+                            <option value="Primeiro" selected>Primeiro</option>
+                            <option value="Segundo">Segundo</option>
+                        </select>
+                        <label for="Andar" class="form-label">Andar</label>
+                    </div>
                 </div>
-            </div>
-            <div class="col-5">
-                <div class="form-floating">
-                    <select class="form-select" id="Tamanho" name="tamanho">
-                        <option value="1" selected>Pequeno</option>
-                        <option value="2">Médio</option>
-                        <option value="3">Grande</option>
-                    </select>
-                    <label for="Tamanho" class="form-label">Tamanho</label>
+                <div class="col-5">
+                    <div class="form-floating">
+                        <select class="form-select" id="Tamanho" name="tamanho">
+                            <option value="1" selected>Pequeno</option>
+                            <option value="2">Médio</option>
+                            <option value="3">Grande</option>
+                        </select>
+                        <label for="Tamanho" class="form-label">Tamanho</label>
+                    </div>
                 </div>
-            </div>
-            <div class="col-2 d-flex justify-content-center" style="height: 50px">
-                <input type="submit" class="btn" value="Filtrar">
-            </div>
-            </form>
-        </div>
-        <div class="container row mt-3">
-            <?php
+                <div class="col-2 d-flex justify-content-center" style="height: 50px">
+                    <input type="submit" class="btn" value="Filtrar">
+                </div>
+        </form>
+    </div>
+    <div class="container row mt-3">
+        <?php
 
-            $controller = new ArmarioController();
+        $controller = new ArmarioController();
 
-            $controller -> SelecionarTodosArmarios();
+        //$controller->SelecionarTodosArmarios();
 
-            if ( isset($_GET['andar']) && isset($_GET['tamanho']) ) {
-                $controller -> FiltrarArmarios($_GET['andar'], $_GET['tamanho']);
-            }
+        if (isset($_GET['andar']) && isset($_GET['tamanho'])) {
+            $controller->FiltrarArmarios($_GET['andar'], $_GET['tamanho']);
+        }
 
-            $armarios = $_REQUEST['armarios'];
+        // PHP Data Objects(PDO) Sample Code:
+        try {
+            $conn = new PDO("sqlsrv:server = tcp:testes-aplicacoes.database.windows.net,1433; Database = testes", "teste", "qwe456123!");
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            print ("Error connecting to SQL Server.");
+            die(print_r($e));
+        }
+
+        // SQL Server Extension Sample Code:
+        $connectionInfo = array("UID" => "teste", "pwd" => "qwe456123!", "Database" => "testes", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+        $serverName = "tcp:testes-aplicacoes.database.windows.net,1433";
+        $conn = sqlsrv_connect($serverName, $connectionInfo);
+        if ($conn === false) {
+            die(print_r(sqlsrv_errors(), true));
+        }
+
+        $armarios = sqlsrv_query($conn, "SELECT * FROM armario");
 
 
-            while($row = sqlsrv_fetch_array($armarios, SQLSRV_FETCH_ASSOC)){
 
-                if ($row['andar'] == $_SESSION['andar'] && $row['idTamanho'] == $_SESSION['tamanho']) {
-                    echo '        
+        while ($row = sqlsrv_fetch_array($armarios, SQLSRV_FETCH_ASSOC)) {
+
+            if ($row['andar'] == $_SESSION['andar'] && $row['idTamanho'] == $_SESSION['tamanho']) {
+                echo '        
                     <div class="card cardArmarios">
-                    <a href="index.php?view=solicitarArmario&idArmario='.$row['idArmario'].'">
-                        <img src="'.$row['imagem'].'" alt="" class="card-img-top">
+                    <a href="index.php?view=solicitarArmario&idArmario=' . $row['idArmario'] . '">
+                        <img src="' . $row['imagem'] . '" alt="" class="card-img-top">
                         <div class="card-body">
-                            <h5 class="card-title">'.$row['nome'].'</h5>
+                            <h5 class="card-title">' . $row['nome'] . '</h5>
                             <p class="card-text"></p>
                         </div>
                         </a>
                     </div>';
-                } else {
-                    
-                }
-                
+            } else {
+
             }
 
-            ?>
+        }
 
-        </div>
+        ?>
+
+    </div>
     </div>
 
     <script src="resources/js/js.js"></script>
 
 </body>
+
 </html>
